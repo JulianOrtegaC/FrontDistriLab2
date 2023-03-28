b19720a04800e4c20f7099a76d15a9ed453fc4a0
MiguelAngel117 <miguel.choconta@uptc.edu.co>
2023-03-27 22:01:57 -0500

    ADD TABLE IN INSCRIPTIONS

--- FrontDistriLab2/src/app/components/inscripcion/inscripcion.component.ts
+++ FrontDistriLab2/src/app/components/inscripcion/inscripcion.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Inscripcion {
+  id_inscripcion: number,
+  cod_materia: number;
+  cod_estudiante: string
+}
+
+const ELEMENT_DATA: Inscripcion[] = [
+  {id_inscripcion: 1, cod_materia: 11121, cod_estudiante: '20191299'},
+  {id_inscripcion: 1, cod_materia: 11121, cod_estudiante: '20191299'},
+  {id_inscripcion: 1, cod_materia: 11121, cod_estudiante: '20191299'},
+  {id_inscripcion: 1, cod_materia: 11121, cod_estudiante: '20191299'},
+  {id_inscripcion: 1, cod_materia: 11121, cod_estudiante: '20191299'},
+];
 
 @Component({
-  selector: 'app-inscripcion',
+  selector: 'app-estudiantes',
   templateUrl: './inscripcion.component.html',
   styleUrls: ['./inscripcion.component.css']
 })
-export class InscripcionComponent {
+export class InscripcionComponent implements OnInit {
+  displayedColumns: string[] = ['ID_INSCRIPCION', 'CODIGO_MATERIA', 'CODIGO_ESTUDIANTE']
+  dataSource: Inscripcion[] = ELEMENT_DATA;
+
+  ngOnInit() {
+  }
 
 }
